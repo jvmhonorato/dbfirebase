5ee0e87397fb3806e9e83049aa02b1ff8c20e5c3
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-25 22:33:09 -0300

    init settings

---
+++ firestore-access/test-auth.js
@@ -0,0 +1,8 @@
+const admin = require("firebase-admin");
+
+//nome e caminho  da chave baixada
+const serviceAccount = require("./firestore.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
