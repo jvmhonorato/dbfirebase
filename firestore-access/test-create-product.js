9d6ad4bc54c0eeca1008a7e446c8844b3d392934
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-26 10:50:58 -0300

    sending data with references

---
+++ firestore-access/test-create-product.js
@@ -0,0 +1,29 @@
+const admin = require("firebase-admin");
+
+//nome e caminho  da chave baixada
+const serviceAccount = require("./firestore.json");
+
+//uni o admin com o cradetialçs e serviceAccount
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+//estanciando admin com as credenciais em firestore() em db
+const db = admin.firestore()
+
+//categoria estanciada
+const cat1 = 'Byi1tIf63GltKlU2VhLS'
+//criar referência entrea categoria estanciada em cat1 com a collection categories 
+const catRef = db.collection('categories').doc(cat1)
+
+//estanciando as collection usando a extenção doc()
+const doc = db.collection('products').doc()
+//set o doc chama o doc e depois o then cria categoria via código la no firebase
+doc.set({
+    product: 'Nome product',
+    price: 2000,
+    categories:[catRef],
+    categories2:[cat1]
+}).then(snap => {
+    console.log(snap)
+})
